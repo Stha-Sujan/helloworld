7773bf35e00d552689545ee1f39fca358063983c
Stha-Sujan <mesujanstha143@gmail.com>
2023-05-03 08:48:53 +0545

    added two files in the repo

---
+++ spam.py
@@ -0,0 +1,13 @@
+import time
+import pyautogui
+
+def SendMessage():
+    time.sleep(5)
+    text = open('message.txt')
+    for each_line in text:
+        pyautogui.typewrite(each_line)
+        time.sleep(0.5)
+        pyautogui.press('enter')
+        
+
+SendMessage()
